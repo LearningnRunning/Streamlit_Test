809aba3f8ae7fcbf53e4bd600de0a9960a63743a
Rocki_Kim <rocki.k1208@gmail.com>
2023-03-02 15:21:32 +0900

    FEAT: various

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import cv2
 import numpy as np
+import pandas as pd
 
 from time import time
 
+save_df = pd.read_csv('result_time.csv')
 
 st.sidebar.header("所要時間テスト")
 
 
 st.title("TEST")
 st.write("画像送受信にかかる時間を計算しようとするテストです。 画像をアップロードしていただくと時間が表示されます。")
+
+region = st.text_input("검색할 지역을 입력해주세요(ex 영등포구 or 속초시)", value="서울특별시 중구")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 start = time()
 
@@ -20,5 +24,10 @@ if uploaded_file is not None:
     # OnlyFace ,pore, OnlyWrinkle = FaceSegmentation(image)
     # result = MakePore(OnlyFace)
     st.image(image, channels="BGR")
-    print(f"{time() - start:.4f} sec")
-    st.write(f"{time() - start:.4f} 秒かかりました。")
+    result_time = f"{time() - start:.4f} sec"
+    st.write(result_time)
+    new_row = pd.DataFrame({"region": [region], "sec": [result_time]})
+    save_df = pd.concat([save_df, new_row])
+    save_df.to_csv('result_time.csv', encoding='utf-8-sig', index=False)
+
+    
